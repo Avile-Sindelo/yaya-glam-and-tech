81e489a20594374e4fb4417275af142a45e73677
Avile Sindelo <avilesindelo@gmail.com>
2026-07-11 12:54:37 +0200

    complete navigability

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ import { engine } from 'express-handlebars';
 import homeRoutes from './routes/home.routes.js';
 import bookingRoutes from './routes/booking.routes.js';
 import adminRoutes from './routes/admin.routes.js';
+import galleryRoutes from "./routes/gallery.routes.js";
+import aboutRoutes from "./routes/about.route.js";
+import contactRoutes from "./routes/contact.routes.js";
 
 const app = express();
 
@@ -21,6 +24,9 @@ app.use(express.static('public'));
 app.use('/', homeRoutes);
 app.use('/bookings', bookingRoutes);
 app.use('/admin', adminRoutes);
+app.use('/gallery', galleryRoutes);
+app.use('/about', aboutRoutes);
+app.use('/contact', contactRoutes);
 
 const PORT = process.env.PORT || 3000;
 
